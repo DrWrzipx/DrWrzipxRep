9776d400a27c2858ad4bc80d0292cb0c61060640
DrWrzipx <matic.drobez@gmail.com>
2018-03-04 19:30:20 +0100

    Some changes from Windows OS

--- example_05.py
+++ example_05.py
@@ -3,16 +3,18 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
-def fetchWords():
+def fetch_words():
     story = urlopen('https://sixty-north.com/c/t.txt')
     story_words = []
     for line in story:
         line_words = line.split()
         for word in line_words:
             story_words.append(word)
+    print_words(story_words)
+
+def print_words(story_words):
     for word in story_words:
         print(word)
 
-
 if __name__ == "__main__":
-    fetchWords()
+    fetch_words()
