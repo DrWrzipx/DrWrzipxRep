2cbd323cfea1c12c700c67d6d3f3f2aa8aa31ffd
DrWrzipx <matic.drobez@gmail.com>
2018-03-04 19:04:50 +0100

    Some changes on bouth files

--- example_05.py
+++ example_05.py
@@ -2,13 +2,17 @@ try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
-story = urlopen('https://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-    line_words = line.split()
-    for word in line_words:
-        story_words.append(word)
+
+def fetchWords():
+    story = urlopen('https://sixty-north.com/c/t.txt')
+    story_words = []
+    for line in story:
+        line_words = line.split()
+        for word in line_words:
+            story_words.append(word)
+    for word in story_words:
+        print(word)
 
 
-for word in story_words:
-    print(word)
+if __name__ == "__main__":
+    fetchWords()
