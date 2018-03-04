efa22eeeaec8ad61b62f09500414033d760d5576
DrWrzipx <matic.drobez@gmail.com>
2018-03-04 18:40:22 +0100

    Change url

--- example_05.py
+++ example_05.py
@@ -2,10 +2,13 @@ try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
-story = urlopen('https://www.python.org')
+story = urlopen('https://sixty-north.com/c/t.txt')
 story_words = []
 for line in story:
     line_words = line.split()
     for word in line_words:
         story_words.append(word)
-print(story_words)
+
+
+for word in story_words:
+    print(word)
