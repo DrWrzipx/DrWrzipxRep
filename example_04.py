6be1912edbe3a91c36a6a23f1d1ef28ecf1a31e7
DrWrzipx <matic.drobez@gmail.com>
2018-03-04 18:15:56 +0100

    Add for loop for easy interaction

--- example_04.py
+++ example_04.py
@@ -1,3 +1,5 @@
+#Data structure: List:
+
 list = [1, 3, 4]
 fruits = ["apple", "orange", "pear"]
 print(list)
@@ -7,3 +9,19 @@ print (fruits)
 
 fruits.append(list)
 print(fruits)
+
+# Data structure: Dictionary:
+
+d = {}
+d = {'alice': '817-673-345', 'bob': '345-245-785', 'mario': '384-048-283'}
+print(d)
+#print(mario)
+
+cities = ['Maribor', 'Celje', 'Velenje', 'Novo Mesto', 'Ljubljana']
+for city in cities:
+    print (city)
+
+print('\n')
+
+for index in d:
+    print(index)
