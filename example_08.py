dfcf3b15ca9bcef4e0521c9799f18074fa531f84
DrWrzipx <matic.drobez@gmail.com>
2018-03-05 20:36:55 +0100

    Add function for dictionary collection

--- example_08.py
+++ example_08.py
@@ -20,10 +20,21 @@ def range_collection(start_number, stop_number, step):
         print(i)
 
 
+def dictionary_collection(dictionary):
+    for item in dictionary:
+        print(item)
+
+
+
 if __name__ == "__main__":
+    urls = {'Google': 'www.google.com',
+            'Pluralsight': 'www.pluralsight.com',
+            'Yahoo': 'www.yahoo.com',
+            'Sixt': 'www.sixt.de'}
     t = ("Norway", 4.56, 3)
     tuple_collection(t)
     string_collection("Anna is so beautiful.", "Matic", "USA")
     range_collection(20, 1000, 40)
+    dictionary_collection(urls)
 
 
