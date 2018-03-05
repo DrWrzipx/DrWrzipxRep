37fd61e1c4459c0cecb474091e292c7a423998db
DrWrzipx <matic.drobez@gmail.com>
2018-03-05 18:44:37 +0100

    Change on file, how to replace values on first 3 indexes

--- example_07.py
+++ example_07.py
@@ -2,16 +2,26 @@ m = [3, 56, 78]
 f = [4, 64, 304]
 
 
+
 def modify(k):
     k.append(39)
     print("k = ", k)
 
 
-def replace(g):
+def replace(g):  # Wrong function
     # g = [17, 28, 45]
+    print("g = ", g)
+
+
+def replace_correct(g):
+    g[0] = 23
+    g[1] = 11
+    g[2] = 23
     print("g = ", g)
 
 
 if __name__ == "__main__":
     modify(m)
     replace(f)
+    print("f = ", f)
+    replace_correct(f)
