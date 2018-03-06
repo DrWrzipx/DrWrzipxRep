a1aac366b4f94eead75253cecfe0d1e219e4f250
DrWrzipx <matic.drobez@gmail.com>
2018-03-06 17:54:46 +0100

    Add new version of Interables

---
+++ example_10.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    words = "Why sometimes I have believed as many as " \
+            "sixt impossible things before breakfast".split()
+    print([len(word) for word in words])
