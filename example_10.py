e136069d76a05a477221704ebfbdda38e28f6c52
DrWrzipx <matic.drobez@gmail.com>
2018-03-06 18:11:10 +0100

    Add some new thing in file

--- example_10.py
+++ example_10.py
@@ -1,4 +1,19 @@
+def gen123():
+    yield 1
+    yield 2
+    yield 3
+
 if __name__ == "__main__":
     words = "Why sometimes I have believed as many as " \
             "sixt impossible things before breakfast".split()
     print([len(word) for word in words])
+
+    iterable = ['Spring', 'Summer', 'Autumn', 'Winter']
+    iterator = iter(iterable)
+    print(next(iterator))
+    for v in gen123():
+        print(v)
+
+    i = gen123()
+    h = gen123()
+    print(i is h) # Two generate generator are not same
