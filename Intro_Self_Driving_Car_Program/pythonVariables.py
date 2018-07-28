3b557411efba94b531c8904cc2394938fe9814d8
Matic Drobez <matic.drobez@gmail.com>
2018-07-28 18:50:02 +0200

    Third commit

---
+++ Intro_Self_Driving_Car_Program/pythonVariables.py
@@ -0,0 +1,8 @@
+# Here we assign a value of 0.5 to the variable named p_heads
+# and then use a print statement.
+p_heads = 0.75
+print("The probability of heads is", p_heads)
+
+# Here we assign a value to p_tails.
+p_tails = 1.0 - p_heads
+print("The probability of tails is", p_tails)
